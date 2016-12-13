0b348a2dcc43853aa3dbb2f8baacfac58059e9ea
Alexandre Tombeur <alexandre.tombeur@gmail.com>
2016-12-13 12:55:15 +0100

    create app + mailchimp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'thankyou', to: 'pages#thankyou'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
